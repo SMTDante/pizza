5a6bada25e8179fffc47bdd3a317f38b7218ddd5
SMTDante <smtdante@gmail.com>
2017-06-27 11:56:37 -0400

    pizza assignment

--- pizza.rb
+++ pizza.rb
@@ -14,10 +14,10 @@ def cheese
 	 cheese = ["cheddar", "feta", "mozzarella", "gouda", "four cheese"]
 end
 
-3.times do
+5.times do
 	p size.sample
 	p crust.sample
 	p meat.sample
 	p cheese.sample
-	puts # placed an empty puts to add a space for each pizza, making it easier to read in PowerShell
+	puts # placed an empty puts to add a space between each individual pizza, making it easier to read in PowerShell
 end
