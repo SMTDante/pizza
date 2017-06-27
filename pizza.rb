491fb392ef25da2f9a24defa80ef63828e8bf830
SMTDante <smtdante@gmail.com>
2017-06-27 11:53:13 -0400

    pizza assignment

---
+++ pizza.rb
@@ -0,0 +1,23 @@
+def size
+	 size = ["personal pan", "medium", "large"]
+end
+
+def crust
+	 crust = ["thin", "thick", "stuffed crust"]
+end
+
+def meat
+	 meat = ["pepperoni", "bacon", "ham", "meat lovers"]
+end
+
+def cheese
+	 cheese = ["cheddar", "feta", "mozzarella", "gouda", "four cheese"]
+end
+
+3.times do
+	p size.sample
+	p crust.sample
+	p meat.sample
+	p cheese.sample
+	puts
+end
